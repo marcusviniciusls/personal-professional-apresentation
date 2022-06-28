f2d514bfbc0dadcbcd8ad771c9185d970d23ab8b
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-06-28 15:56:34 -0300

    Entity

---
+++ src/main/java/br/com/marcus/dev/personal/professiona/apresentaion/entities/BranchActivity.java
@@ -0,0 +1,18 @@
+package br.com.marcus.dev.personal.professiona.apresentaion.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "tb_branch_activity")
+public class BranchActivity {
+
+    private String name;
+}
---
+++ src/main/java/br/com/marcus/dev/personal/professiona/apresentaion/entities/Partner.java
@@ -0,0 +1,16 @@
+package br.com.marcus.dev.personal.professiona.apresentaion.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "tb_partner")
+public class Partner {
+}
