5f56efcb337bddd9c516a0105bfbf1668dd6fc6a
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-07-01 15:11:47 -0300

    entity finalizada

--- src/main/java/br/com/marcus/dev/personal/professiona/apresentaion/entities/StudyPlan.java
+++ src/main/java/br/com/marcus/dev/personal/professiona/apresentaion/entities/StudyPlan.java
@@ -28,11 +28,35 @@ public class StudyPlan extends SuperEntity{
     @OneToMany(mappedBy = "studyPlan")
     private List<Topic> listTopic = new ArrayList<>();
 
-    //ManyToMany
-    //@JoinTable(name = "studyplan_course", joinColumns =
-    //        {@JoinColumn(name = "study_plan_id")}, inverseJoinColumns =
-    //        {@JoinColumn(name="course_id")})
-    //private List<Course> listCourse = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(name = "tb_studyplan_course", joinColumns =
+            {@JoinColumn(name = "study_plan_id")}, inverseJoinColumns =
+            {@JoinColumn(name="course_id")})
+    private List<Course> listCourse = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_studyplan_professionalExperience", joinColumns =
+            {@JoinColumn(name = "study_plan_id")}, inverseJoinColumns =
+            {@JoinColumn(name="professional_experience_id")})
+    private List<ProfessionalExperience> listProfessionalExperience = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_studyplan_assignments", joinColumns =
+            {@JoinColumn(name = "study_plan_id")}, inverseJoinColumns =
+            {@JoinColumn(name="assignments_id")})
+    private List<Assignments> listAssignments = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_studyplan_graduation", joinColumns =
+            {@JoinColumn(name = "study_plan_id")}, inverseJoinColumns =
+            {@JoinColumn(name="graduation_id")})
+    private List<Graduation> listGraduation = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_studyplan_certificate", joinColumns =
+            {@JoinColumn(name = "study_plan_id")}, inverseJoinColumns =
+            {@JoinColumn(name="certificate_id")})
+    private List<Certificate> listCertificate = new ArrayList<>();
 
     public void addListTopic(Topic topic){
         this.listTopic.add(topic);
