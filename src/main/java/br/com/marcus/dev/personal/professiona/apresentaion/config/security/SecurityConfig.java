c9fa55d9de4d8605b7b1c66fafcb1244a3083da3
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-08-06 14:09:14 -0300

    Publico login

--- src/main/java/br/com/marcus/dev/personal/professiona/apresentaion/config/security/SecurityConfig.java
+++ src/main/java/br/com/marcus/dev/personal/professiona/apresentaion/config/security/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private JWTUtil jwtUtil;
 
     private static final String[] PUBLIC_MATCHERS = {
-            "/h2-console/**"
+            "/h2-console/**",
+            "/login/**"
     };
 
     private static final String[] PUBLIC_MATCHERS_GET = {
