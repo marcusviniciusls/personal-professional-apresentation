a1b0c6a05eff3754f490638d60e967deb6b67ae6
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-08-09 20:49:57 -0300

    Finalizacao de Seguranca

--- src/main/java/br/com/marcus/dev/personal/professional/apresentation/config/security/SecurityConfig.java
+++ src/main/java/br/com/marcus/dev/personal/professional/apresentation/config/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,12 +16,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import springfox.documentation.service.ApiKey;
 
 import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
--- src/main/java/br/com/marcus/dev/personal/professional/apresentation/controller/BranchActivityController.java
+++ src/main/java/br/com/marcus/dev/personal/professional/apresentation/controller/BranchActivityController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class BranchActivityController {
 
     @Autowired private FindAllBranchActivityService findAllBranchActivityService;
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping
     public ResponseEntity<Page<BranchActivityResponse>> findAllBranchActivity(Pageable page){
         Page<BranchActivityResponse> branchActivityResponse = findAllBranchActivityService.findAll(page);
