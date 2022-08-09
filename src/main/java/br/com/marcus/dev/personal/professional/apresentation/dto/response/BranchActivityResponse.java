a58bb4e7d65138c2c601c8efe51a6152ba7447e6
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-08-09 20:09:51 -0300

    Lista de todas as branch commit

--- src/main/java/br/com/marcus/dev/personal/professional/apresentation/dto/response/BranchActivityResponse.java
+++ src/main/java/br/com/marcus/dev/personal/professional/apresentation/dto/response/BranchActivityResponse.java
@@ -3,10 +3,12 @@ package br.com.marcus.dev.personal.professional.apresentation.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class BranchActivityResponse {
 
     private String name;
--- src/main/java/br/com/marcus/dev/personal/professional/apresentation/services/branchactivity/factory/BranchActivityFactory.java
+++ src/main/java/br/com/marcus/dev/personal/professional/apresentation/services/branchactivity/factory/BranchActivityFactory.java
@@ -2,15 +2,14 @@ package br.com.marcus.dev.personal.professional.apresentation.services.branchact
 
 import br.com.marcus.dev.personal.professional.apresentation.dto.response.BranchActivityResponse;
 import br.com.marcus.dev.personal.professional.apresentation.entities.BranchActivity;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BranchActivityFactory {
 
-    private ModelMapper modelMapper = new ModelMapper();
-
     public BranchActivityResponse convertEntityInResponse(BranchActivity branchActivity){
-        return modelMapper.map(branchActivity, BranchActivityResponse.class);
+        BranchActivityResponse response =  new BranchActivityResponse();
+        response.setName(branchActivity.getName());
+        return response;
     }
 }
