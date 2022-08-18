17d7772bbeb0d80597513bfdcfeb816ed4726561
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-08-17 23:43:29 -0300

    Deletar Email incompleto

---
+++ src/main/java/br/com/marcus/dev/personal/professional/apresentation/services/email/DeleteEmailService.java
@@ -0,0 +1,9 @@
+package br.com.marcus.dev.personal.professional.apresentation.services.email;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteEmailService {
+
+    public void delete()
+}
