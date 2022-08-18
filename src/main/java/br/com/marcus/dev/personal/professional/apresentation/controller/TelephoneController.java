14c8aee93a714bcb57d8e96044d7471ab1fe15e2
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-08-18 20:12:24 -0300

    Buscar por id o telephone

--- src/main/java/br/com/marcus/dev/personal/professional/apresentation/controller/TelephoneController.java
+++ src/main/java/br/com/marcus/dev/personal/professional/apresentation/controller/TelephoneController.java
@@ -3,23 +3,34 @@ package br.com.marcus.dev.personal.professional.apresentation.controller;
 import br.com.marcus.dev.personal.professional.apresentation.dto.response.EmailDto;
 import br.com.marcus.dev.personal.professional.apresentation.dto.response.TelephoneDto;
 import br.com.marcus.dev.personal.professional.apresentation.services.telephone.FindAllTelephoneService;
+import br.com.marcus.dev.personal.professional.apresentation.services.telephone.FindByIdTelephoneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value="/telephone")
 public class TelephoneController {
 
     @Autowired private FindAllTelephoneService findAllTelephoneService;
+    @Autowired private FindByIdTelephoneService findByIdTelephoneService;
 
     @GetMapping
     public ResponseEntity<Page<TelephoneDto>> findAll(Pageable page){
         Page<TelephoneDto> telephoneDto = findAllTelephoneService.findAll(page);
+        return ResponseEntity.ok().body(telephoneDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<TelephoneDto> findById(@PathVariable UUID id){
+        TelephoneDto telephoneDto = findByIdTelephoneService.findById(id);
         return ResponseEntity.ok().body(telephoneDto);
     }
 }
---
+++ src/main/java/br/com/marcus/dev/personal/professional/apresentation/services/telephone/FindByIdTelephoneService.java
@@ -0,0 +1,36 @@
+package br.com.marcus.dev.personal.professional.apresentation.services.telephone;
+
+import br.com.marcus.dev.personal.professional.apresentation.dto.response.EmailDto;
+import br.com.marcus.dev.personal.professional.apresentation.dto.response.TelephoneDto;
+import br.com.marcus.dev.personal.professional.apresentation.entities.Email;
+import br.com.marcus.dev.personal.professional.apresentation.entities.Telephone;
+import br.com.marcus.dev.personal.professional.apresentation.exception.custom.ResourceNotFoundException;
+import br.com.marcus.dev.personal.professional.apresentation.repository.TelephoneRepository;
+import br.com.marcus.dev.personal.professional.apresentation.services.CenterEntityService;
+import br.com.marcus.dev.personal.professional.apresentation.services.telephone.factory.TelephoneFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class FindByIdTelephoneService {
+
+    @Autowired private TelephoneRepository telephoneRepository;
+    @Autowired private CenterEntityService centerEntityService;
+    @Autowired private TelephoneFactory telephoneFactory;
+
+    public TelephoneDto findById(UUID id){
+        Optional<Telephone> optionalTelephone = telephoneRepository.findById(id);
+        if (optionalTelephone.isEmpty()){
+            throw new ResourceNotFoundException("ID Not Found Exception");
+        }
+        Telephone telephone = optionalTelephone.get();
+        if (!centerEntityService.isStatusSuperEntity(telephone)){
+            throw new ResourceNotFoundException("ID Not Found Exception");
+        }
+        TelephoneDto telephoneDto = telephoneFactory.convertEntityInDto(telephone);
+        return telephoneDto;
+    }
+}
