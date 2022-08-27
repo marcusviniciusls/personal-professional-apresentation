dd94371df7b8aac70273fcc9c1ddf22abc4fefb6
Marcus Vinicius Lima Silva <viniciusmls@outlook.com>
2022-08-27 11:43:46 -0300

    Enviar imagem para o bucket

--- src/main/java/br/com/marcus/dev/personal/professional/apresentation/config/amazon/S3Service.java
+++ src/main/java/br/com/marcus/dev/personal/professional/apresentation/config/amazon/S3Service.java
@@ -1,15 +1,19 @@
 package br.com.marcus.dev.personal.professional.apresentation.config.amazon;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.services.s3.AmazonS3;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.amazonaws.services.s3.AmazonS3;
 
 import java.io.File;
 
@@ -19,23 +23,23 @@ public class S3Service {
 
     private Logger LOG = LoggerFactory.getLogger(S3Service.class);
 
-    @Autowired private S3Config s3Config;
-    @Autowired private AmazonS3 amazonS3Client;
+    @Autowired private AmazonS3 s3Client;
     @Value("${s3.bucket}")
     private String bucketName;
 
     public void uploadFile(String localFilePath){
         try{
-            amazonS3Client = s3Config.s3Client();
             File file = new File(localFilePath);
             LOG.info("Iniciando Upload");
-            amazonS3Client.putObject(bucketName, "teste", file);
+            s3Client.putObject(bucketName, "teste", file);
             LOG.info("Finalizando Upload");
         } catch(AmazonServiceException amazonServiceException){
             LOG.info("amazonServiceException " + amazonServiceException.getErrorMessage());
             LOG.info("Status Code: " + amazonServiceException.getStatusCode());
+            amazonServiceException.printStackTrace();
         } catch(AmazonClientException amazonClientException){
             LOG.info("amazonClientException " + amazonClientException.getMessage());
+            amazonClientException.printStackTrace();
         }
     }
 }
